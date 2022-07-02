398e137f9225f67becf7a84a0b9f54351d958ec8
wisekim <lwisekiml@naver.com>
2022-07-02 22:31:35 +0900

    프로젝트 생성

---
+++ src/main/java/Exercise/ExerciseWeb/ExerciseWebApplication.java
@@ -0,0 +1,13 @@
+package Exercise.ExerciseWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Exercise/ExerciseWeb/ExerciseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Exercise.ExerciseWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
