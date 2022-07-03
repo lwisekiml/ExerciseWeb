c3460ede65f3b044e937bfedbfd7c0e1cdd5c4d5
wisekim <lwisekiml@naver.com>
2022-07-03 22:51:36 +0900

    package name modify

--- src/main/java/com/exercise/ExerciseWebApplication.java
+++ src/main/java/com/exercise/ExerciseWebApplication.java
@@ -1,4 +1,4 @@
-package Exercise.ExerciseWeb;
+package com.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/exercise/ExerciseWebApplicationTests.java
+++ src/test/java/com/exercise/ExerciseWebApplicationTests.java
@@ -1,4 +1,4 @@
-package Exercise.ExerciseWeb;
+package com.exercise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
